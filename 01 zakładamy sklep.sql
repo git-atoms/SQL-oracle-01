cef745ef1c2621efdb9a09fd359f6029a511979f
git-atoms <atom.sliwinski@gmail.com>
2021-09-03 21:38:06 +0200

    Update 01 zakładamy sklep.sql

--- 01 zakładamy sklep.sql
+++ 01 zakładamy sklep.sql
@@ -1,1 +1,9 @@
+--Tworzenie nowego u¿ytkownika
 create user sklep IDENTIFIED by 123;
+
+--Przyznanie uprawnieñ u¿ytkownikowi
+grant all privileges to sklep;
+
+
+--Sprawdzenie na jakiego u¿ytkownika jesteœmy zalogowani
+select user from dual;
