23d0f34a3682f2e4cba8a9878e96f3d2eea5fd86
git-atoms <atom.sliwinski@gmail.com>
2021-09-17 18:28:24 +0200

    Create 03pracownicy-wprowadzanie danych.sql

---
+++ 03pracownicy-wprowadzanie danych.sql
@@ -0,0 +1,39 @@
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(1,'Jan','Kowalski',88020512345,'M','88/02/05',4000);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(2,'Krzysztof','Kapusta',70122012345,'M','70/12/20',2500);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(3,'Magdalena','Dzwonkowska',92081612345,'K','92/08/16',5000);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(4,'Karol','Krawczyk',82010112345,'M','82/01/01',3000);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(5,'Joanna','Bak',90121212345,'K','90/12/12',2700);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(6,'Robert','Psikuta',95070815642,'M','95/07/08',4200);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(7,'Stefan','Siarzewski',60051274391,'M','60/05/12',4550);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(8,'Zofia','Brzydal',84021659712,'K','84/02/16',2400);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(9,'Julia','Niedziela',81050242315,'K','81/05/02',7000);
+
+insert into pracownicy (
+id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
+values(10,'Bartosz','Karmel',58031245679,'M','58/03/12',5120);
