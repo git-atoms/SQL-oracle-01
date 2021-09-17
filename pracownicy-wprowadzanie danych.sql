128437f8e0fdc8a5c0b862d61a1ce1d8a31e56f6
git-atoms <atom.sliwinski@gmail.com>
2021-09-17 18:14:16 +0200

    Update pracownicy-wprowadzanie danych.sql

--- pracownicy-wprowadzanie danych.sql
+++ pracownicy-wprowadzanie danych.sql
@@ -20,20 +20,20 @@ values(5,'Joanna','Bak',90121212345,'K','90/12/12',2700);
 
 insert into pracownicy (
 id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
-values(6,'Ste','',,'','',);
+values(6,'Robert','Psikuta',95070815642,'M','95/07/08',4200);
 
 insert into pracownicy (
 id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
-values(7,'','',,'','',);
+values(7,'Stefan','Siarzewski',60051274391,'M','60/05/12',4550);
 
 insert into pracownicy (
 id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
-values(8,'','',,'','',);
+values(8,'Zofia','Brzydal',84021659712,'K','84/02/16',2400);
 
 insert into pracownicy (
 id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
-values(9,'','',,'','',);
+values(9,'Julia','Niedziela',81050242315,'K','81/05/02',7000);
 
 insert into pracownicy (
 id, imie, nazwisko, pesel, plec, data_urodzenia,pensja)
-values(10,'','',,'','',);
+values(10,'Bartosz','Karmel',58031245679,'M','58/03/12',5120);
